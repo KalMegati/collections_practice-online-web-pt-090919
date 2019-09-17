5d55a4ba8cf9dc8ac1f2b80b0e0471160a8caa51
Srikalyan Megati <kalmegati@gmail.com>
2019-09-17 16:56:32 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,7 +10,7 @@ def sort_array_char_count(array)
   array.sort { |x, y| x.length <=> y.length }
 end
 
-def swap_elements(array)
+def swap_elements(array, origin, destination)
   array.sort { |x, y| 
     if array.index(x) == 1
       1
