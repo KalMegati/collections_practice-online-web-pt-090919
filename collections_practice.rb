3625271a39f6f1c218cb0753c86715a3168b6e12
Srikalyan Megati <kalmegati@gmail.com>
2019-09-17 16:52:28 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,21 @@
+def sort_array_asc(array)
+  array.sort { |x, y| x <=> y }
+end
 
+def sort_array_desc(array)
+  array.sort { |x, y| y <=> x }
+end
+
+def sort_array_char_count(array)
+  array.sort { |x, y| x.length <=> y.length }
+end
+
+def swap_elements(array)
+  array.sort { |x, y| 
+    if array.index(x) == 1
+      -1
+    else
+      1
+    end
+  }
+end
